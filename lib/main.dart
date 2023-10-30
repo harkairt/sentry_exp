e128d667f4879e7a1353f07a1c1eacc5803b9897
harkairt <32367206+harkairt@users.noreply.github.com>
2023-10-30 19:50:46 +0100

    1.0.0+7

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'package:sentry_flutter/sentry_flutter.dart';
 
-const release = "1.0.0+6";
+const release = "sentry-exp@1.0.0+7";
 
 Future<void> main() async {
   await SentryFlutter.init(
